dea7ac60a926f4e941d49bb1af48ba39d75cbe05
Stefan Klinger <stefanklinger@yahoo.com>
2012-05-21 21:47:50 +0200

    project setup

--- src/main/java/org/robotrader/quote/domain/Stock.java
+++ src/main/java/org/robotrader/quote/domain/Stock.java
@@ -1,10 +1,12 @@
 package org.robotrader.quote.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Stock {
 
+	@Id
 	private String code;
 
 	public void setCode(String code) {
